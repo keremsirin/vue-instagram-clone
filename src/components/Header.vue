d83da9097ef2e64315f44d3faf72ae01e70fb8f9
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-02-22 21:06:32 +0300

    master page created

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/direct">Direct Messages</router-link> |
+    <router-link to="/explore">Explore</router-link> |
+    <router-link to="/profile">Profile</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MasterHeader'
+}
+</script>
+
+<style scoped></style>
