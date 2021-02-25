d7297b2e8edf34b0886df907075601515db029a9
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-02-25 20:19:54 +0300

    container component fixed

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div class="app">
     <MasterHeader></MasterHeader>
     <main class="main">
-      <Container />
-      <router-view />
+      <Container>
+        <router-view />
+      </Container>
     </main>
   </div>
 </template>
 
 <script>
 import MasterHeader from '@/components/Header'
+import Container from '@/components/Container'
 
 export default {
   name: 'App',
   components: {
-    MasterHeader
+    MasterHeader,
+    Container
   }
 }
 </script>
